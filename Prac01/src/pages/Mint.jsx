a77eb887456fcfdfe99217701c65f691643fa9a0
jkpark125 <jkpark125@gmail.com>
2025-02-03 15:00:48 +0900

    update mint page

--- Prac01/src/pages/Mint.jsx
+++ Prac01/src/pages/Mint.jsx
@@ -1,22 +1,65 @@
+import { useState, useEffect } from "react";
 import { useOutletContext } from "react-router-dom";
-import { ethers } from "ethers";
+import axios from "axios";
 
 function MintPage() {
+  const [tokenId, setTokenId] = useState(1);
+  const [amount, setAmount] = useState(1);
   const {signer, contract} = useOutletContext();
+
+  const [metadata, setMetadata] = useState();
+  const [balance, setBalance] = useState(0);
+  const [addedAmount, setAddedAmount] = useState(0);
 
   const mint = async () => {
     try{
       if(!signer || !contract) return;
-      const res = await contract.mintNFT(7, 100);
-      console.log(res);
-  
+      // await contract.mintNFT(tokenId, amount);
+      
+      const uri = await contract.uri(tokenId);
+      const res = await axios.get(uri);
+      setMetadata(res.data);
+
+      const balance = await contract.balanceOf(signer.address, tokenId);
+      setBalance(Number(balance));
+      setAddedAmount(amount);
+
     } catch (e) {
       console.log(e);
     }
   }
   
-  return <div className="bg-red-100 min-h-screen">
-    <button className={`${signer ? "btn-style " : "btn-disable"}`} disabled={!signer} onClick={mint}>Mint</button>
+  return <div className="bg-amber-100 py-8 flex-col flex items-center">
+    <p className="text-3xl font-bold pb-8">Mint Your Puzzle NFT!</p>
+    <div className="flex-row flex items-center">
+      <p className="text-xl pr-2">ID</p>
+      <input
+        className="input-style"
+        type="number"
+        min={1}
+        max={16}
+        value={tokenId}
+        onChange={(e)=>setTokenId(e.target.value)}
+      />
+      <p className="text-xl pl-4 pr-2">Amount</p>
+      <input
+      className="input-style mx-2"
+        type="number"
+        min={0}
+        max={10}
+        value={amount}
+        onChange={(e)=>setAmount(e.target.value)}
+      />
+      <button className={`${signer ? "btn-style " : "btn-disable"}`} disabled={!signer} onClick={mint}>Mint</button>
+    </div>
+    {metadata
+    ? <div className="flex-col flex items-center">
+      <p className="text-2xl font-bold pt-16 pb-8">You got a piece of puzzle ({tokenId}/16)</p>
+      <img src={metadata.image} alt={metadata.name} />
+      <p>you have collected {balance + addedAmount} pieces</p>
+      
+    </div>
+    :<></>}
   </div>
 }
 
